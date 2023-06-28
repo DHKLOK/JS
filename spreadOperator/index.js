4e4966540a7feef7e77fc9b2b4aea3a32efc2336
DHKLOKlaptop <kwrifles@gmail.com>
2023-06-28 11:52:21 -0400

    Nano Machines son

---
+++ spreadOperator/index.js
@@ -0,0 +1,20 @@
+//Spread operator -----------------------------
+// allows an iterable such as an array or string to be expanded
+// in places where zero or more arguments are expected
+// (unpacks the elements)
+
+let numbers = [1,2,3,4,5,6,7,8,9];
+console.log(...numbers)
+
+let maximum = Math.max(...numbers);
+console.log(maximum);
+
+let class1 = ["spongebob", "patrick", "Sandy"];
+let class2 = ["Mr.Krabs", "Squidward", "Plankton"];
+
+class1.push(...class2);
+
+console.log(class1);
+
+console.log(...class1)
+
