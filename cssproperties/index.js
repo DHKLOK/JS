8a1c318733f707e23465a9b958398c4b2ffd795f
DHKLOKlaptop <kwrifles@gmail.com>
2023-09-20 22:23:16 -0400

    back in the game

---
+++ cssproperties/index.js
@@ -0,0 +1,13 @@
+const title = document.getElementById("myTitle");
+
+title.style.backgroundColor = "blue";
+title.style.color = "rgb(50,200,250)";
+
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+
+title.style.border = "30px solid";
+title.style.borderColor = "black";
+
+
+
